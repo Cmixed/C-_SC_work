7a12c1433689126a16e9f43f850ed44f0766aad2
ZZJ Cmxxd <cmxxdzzj@qq.com>
2024-04-21 15:43:52 +0800

    24

--- 销售员销售系统/FileIO.cpp
+++ 销售员销售系统/FileIO.cpp
@@ -1,9 +1,9 @@
 // 将文本文件中特定月份的数据都存储在一个数组中
-// 
+// 格式：arr[月份的天数][销售员代号][商品的代号]
 
 const int SALES_NUMBER = 5;
 const int GOODS_NUMBER = 5;
 
 
-int arr[31][SALES_NUMBER][GOODS_NUMBER];
+int arr[31][SALES_NUMBER][GOODS_NUMBER] = { 0 };
 
